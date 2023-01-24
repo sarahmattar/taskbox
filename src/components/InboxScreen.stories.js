f04d00b3786eadce1a5f7cb213083c26d789a9b3
Sarah Mattar <sarah.mattar@mac.com>
2023-01-23 20:31:07 -0800

    Add mockserviceworker

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import InboxScreen from './InboxScreen';
+import store from '../lib/store';
+import { rest } from 'msw';
+import { MockedState } from './TaskList.stories';
+
+import { Provider } from 'react-redux';
+
+export default {
+	component: InboxScreen,
+	title: 'InboxScreen',
+	decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+};
+
+const Template = () => <InboxScreen />;
+
+export const Default = Template.bind({});
+
+Default.parameters = {
+	msw: {
+		handlers: [
+			rest.get(
+				'https://jsonplaceholder.typicode.com/todos?userId=1',
+				(req, res, ctx) => {
+					//this fills the tasks array?
+					return res(ctx.json(MockedState.tasks));
+				}
+			),
+		],
+	},
+};
+
+export const Error = Template.bind({});
+
+Error.parameters = {
+	msw: {
+		handlers: [
+			rest.get(
+				'https://jsonplaceholder.typicode.com/todos?userId=1',
+				(req, res, ctx) => {
+					console.log(ctx);
+					//this handles errors accessin|g the typicode api
+					return res(ctx.status(403));
+				}
+			),
+		],
+	},
+};
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,16 +1,31 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import {
+	configureStore,
+	createSlice,
+	createAsyncThunk,
+} from '@reduxjs/toolkit';
 
-const defaultTasks = [
-	{ id: '1', title: 'Something', state: 'TASK_INBOX' },
-	{ id: '2', title: 'Something more', state: 'TASK_INBOX' },
-	{ id: '3', title: 'Something else', state: 'TASK_INBOX' },
-	{ id: '4', title: 'Something again', state: 'TASK_INBOX' },
-];
 const TaskBoxData = {
-	tasks: defaultTasks,
+	tasks: [],
 	status: 'idle',
 	error: null,
 };
+
+// create async thunk to get tasks from remote endpoint
+
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+	const response = await fetch(
+		'https://jsonplaceholder.typicode.com/todos?userId=1'
+	);
+
+	const data = await response.json();
+	const result = data.map((task) => ({
+		id: `${task.id}`,
+		title: task.title,
+		state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+	}));
+
+	return result;
+});
 
 const TasksSlice = createSlice({
 	name: 'taskbox',
@@ -23,6 +38,25 @@ const TasksSlice = createSlice({
 				state.tasks[task].state = newTaskState;
 			}
 		},
+	},
+	// extends the fetchTasks reducer to handle asynchronous responses
+	extraReducers(builder) {
+		builder
+			.addCase(fetchTasks.pending, (state) => {
+				state.status = 'loading';
+				state.error = null;
+				state.tasks = [];
+			})
+			.addCase(fetchTasks.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.error = null;
+				state.tasks = action.payload;
+			})
+			.addCase(fetchTasks.rejected, (state) => {
+				state.status = 'failed';
+				state.error = 'Something went wrong';
+				state.tasks = [];
+			});
 	},
 });
 
